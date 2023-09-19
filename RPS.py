f56f005d1557e68d04fa5c44ff0d8638963ea8fa
Oskar Hofmann <oskar.hofmann@gmx.de>
2023-09-19 23:27:50 +0200

    Change function to accept arbitrary number of moves to consider

--- RPS.py
+++ RPS.py
@@ -5,21 +5,23 @@ import itertools
 POSSIBLE_MOVES = ['R', 'P', 'S']
 IDEAL_RESPONSE = {'P': 'S', 'R': 'P', 'S': 'R'}
 
-# all possible 3 move sequences 
-POSSIBLE_3_MOVES_SEQUENCES = list(itertools.product('RPS', repeat = 3))
 
-#learns from opponent history to predict a move based on the opponents previous 3 moves
-def q_learning_3_moves(opponent_history: list[str]) -> str:
+
+#learns from opponent history to predict a move based on the opponents previous n moves
+def q_learning_enemy_moves(opponent_history: list[str], n_moves: int = 3) -> str:
 
     def calc_reward(prediction: int, truth: int) -> int:
         if prediction == truth:
             return 1
         else:
             return -1
+        
+    # all possible n move sequences 
+    POSSIBLE_N_MOVES_SEQUENCES = list(itertools.product('RPS', repeat = n_moves))
     
-    # state: oponnent's move at round n, n-1, n-2
+    # state: oponnent's move at round n, n-1, n-2 ... n-(steps-1)
     # action: (prediction of) opponent's move at round n+1
-    STATES = len(POSSIBLE_3_MOVES_SEQUENCES) # 27
+    STATES = len(POSSIBLE_N_MOVES_SEQUENCES)
     ACTIONS = 3 # R, P, S
 
     # Q = np.zeros((STATES, ACTIONS))
@@ -34,70 +36,26 @@ def q_learning_3_moves(opponent_history: list[str]) -> str:
     considered_opponent_history = opponent_history[-HISTORY_STEPS:]
 
     # if the oponent history contains less than 4 moves, then no real prediction can be made
-    if len(considered_opponent_history) < 4:
+    if len(considered_opponent_history) < (n_moves + 1):
         return 'P'
 
     # iterate over history of oponnent' moves
-    for idx, move in enumerate(considered_opponent_history[:-3]):
-        # build 3 move sequence
-        move_sequence = (considered_opponent_history[idx], considered_opponent_history[idx+1], considered_opponent_history[idx+2])        
+    for idx, move in enumerate(considered_opponent_history[:-n_moves]):
+        # build N move sequence
+        move_sequence = tuple([considered_opponent_history[i] for i in range(idx, idx+n_moves)])    
         # get index of Q table
-        state = POSSIBLE_3_MOVES_SEQUENCES.index(move_sequence)
+        state = POSSIBLE_N_MOVES_SEQUENCES.index(move_sequence)
         # the correct action is the next move of the opponent that should be predicted
-        correct_action = POSSIBLE_MOVES.index(considered_opponent_history[idx+3])
+        correct_action = POSSIBLE_MOVES.index(considered_opponent_history[idx+n_moves])
         # for each possible action, update Q-table based on whether the prediction would be right or wrong
         for action in range(ACTIONS):
             reward = calc_reward(action, correct_action)
             Q[state,action] = (1-LEARNING_RATE) * Q[state,action] + LEARNING_RATE * reward
 
-    last_3_moves = tuple(considered_opponent_history[-3:])
-    calculated_move_index = np.argmax(Q[POSSIBLE_3_MOVES_SEQUENCES.index(last_3_moves), :])
+    last_n_moves = tuple(considered_opponent_history[-n_moves:])
+    calculated_move_index = np.argmax(Q[POSSIBLE_N_MOVES_SEQUENCES.index(last_n_moves), :])
     calculated_move = POSSIBLE_MOVES[calculated_move_index]
 
-    return IDEAL_RESPONSE[calculated_move]
-    
-
-#learns from opponent history to predict a move based on the opponents previous move
-def q_learning_1_move(opponent_history: list[str]) -> str:
-
-    def calc_reward(prediction: int, truth: int) -> int:
-        if prediction == truth:
-            return 1
-        else:
-            return -1
-    
-    # state: oponnent's move at round n
-    # action: (prediction of) opponent's move at round n+1
-    STATES = 3 # R, P, S
-    ACTIONS = 3 # R, P, S
-
-    Q = np.random.rand(STATES, ACTIONS)
-
-    LEARNING_RATE = 0.95
-    # GAMMA = 0.96
-    HISTORY_STEPS = 10 # how many of the previous moves of the opponent should be used for learning? 0 means all steps
-
-    # this uses only the last HISTORY_STEPS entries of opponent_history
-    # still works if HISTORY_STEPS > opponent_history.size or if HISTORY_STEPS = 0 (just uses all entries in both cases)
-    considered_opponent_history = opponent_history[-HISTORY_STEPS:]
-
-    # if the oponent history only contains 1 move, then no real prediction can be made
-    if len(considered_opponent_history) < 2:
-        return 'R'
-    
-    # iterate over history of oponnent' moves
-    for idx, move in enumerate(considered_opponent_history[:-1]):
-        # get index of Q table
-        state = POSSIBLE_MOVES.index(move)
-        # the correct action is the next move of the opponent that should be predicted
-        correct_action = POSSIBLE_MOVES.index(considered_opponent_history[idx+1])
-        # for each possible action, update Q-table based on whether the prediction would be right or wrong
-        for action in range(ACTIONS):
-            reward = calc_reward(action, correct_action)
-            Q[state,action] = (1-LEARNING_RATE) * Q[state,action] + LEARNING_RATE * reward
-
-    calculated_move_index = np.argmax(Q[POSSIBLE_MOVES.index(considered_opponent_history[-1]), :])
-    calculated_move = POSSIBLE_MOVES[calculated_move_index]
     return IDEAL_RESPONSE[calculated_move]
 
 
@@ -109,7 +67,7 @@ def player(prev_play, opponent_history=[]):
         opponent_history = []
 
     # return q_learning_1_move(opponent_history)
-    return q_learning_3_moves(opponent_history)
+    return q_learning_enemy_moves(opponent_history, 3)
 
 #TODO:
 # 1) player uses Q_table = [Q] and only calculates based on latest move
