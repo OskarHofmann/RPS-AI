dffbbff355f6b765eac6b57a9e4a479d6c231c79
Oskar Hofmann <oskar.hofmann@gmx.de>
2023-09-28 01:00:19 +0200

    Fix typing

--- RPS.py
+++ RPS.py
@@ -1,5 +1,6 @@
 import numpy as np
 import itertools
+from typing import Union
 
 # ideal response based on a predicted move of the oponnent
 POSSIBLE_MOVES = ['R', 'P', 'S']
@@ -20,7 +21,7 @@ def initialize_q_table(n_states: int) -> np.ndarray:
     return Q
 
 
-def predict_move(Q: np.ndarray, previous_steps: list[str], possible_n_moves_sequences: list[tuple]) -> str:
+def predict_move(Q: np.ndarray, previous_steps: list[str], possible_n_moves_sequences: list[tuple]) -> tuple[str, float]:
     last_n_moves = tuple(previous_steps)
     calculated_move_index = np.argmax(Q[possible_n_moves_sequences.index(last_n_moves), :])
     calculated_move_prob = Q[possible_n_moves_sequences.index(last_n_moves), calculated_move_index]
@@ -44,7 +45,7 @@ def update_q_table(Q: np.ndarray, previous_steps: list[str], true_outcome: str, 
 
 
 #learns from opponent history to predict a move based on the opponents previous n moves
-def q_learning_enemy_moves(opponent_history: list[str], n_moves: int = 3, q_table = list[np.ndarray]) -> str:
+def q_learning_enemy_moves(opponent_history: list[str], q_table: list[np.ndarray], n_moves: int = 3) -> tuple[str, float]:
 
     # all possible n move sequences 
     possible_n_moves_sequences = list(itertools.product('RPS', repeat = n_moves))
@@ -55,7 +56,7 @@ def q_learning_enemy_moves(opponent_history: list[str], n_moves: int = 3, q_tabl
 
     # if the oponent history contains less than n moves, then no real prediction can be made
     if len(opponent_history) < (n_moves + 1):
-        return 'P'
+        return 'P', 0
     
     if q_table:
         Q = q_table[0]
@@ -70,16 +71,48 @@ def q_learning_enemy_moves(opponent_history: list[str], n_moves: int = 3, q_tabl
     update_q_table(Q, move_sequence_training, correct_action, possible_n_moves_sequences)
 
     last_steps = [opponent_history[i] for i in range(-n_moves , 0)]
-    calculated_move =  predict_move(Q, last_steps, possible_n_moves_sequences)
+    calculated_move, calculated_move_prob =  predict_move(Q, last_steps, possible_n_moves_sequences)
 
-    return IDEAL_RESPONSE[calculated_move]
+    return IDEAL_RESPONSE[calculated_move], calculated_move_prob
+
+
+#learns from history of own and enemy plays to predict a move based on the own previous n moves
+def q_learning_own_moves(opponent_history: list[str], player_history: list[str], q_table: list[np.ndarray], n_moves: int = 2) -> tuple[str, float]:
+
+    # all possible n move sequences 
+    possible_n_moves_sequences = list(itertools.product('RPS', repeat = n_moves))
+    # state: move at round n, n-1, n-2 ... n-(steps-1)
+    # action: (prediction of) move at round n+1
+    n_states = len(possible_n_moves_sequences) 
+        
+
+    # if the oponent history contains less than n moves, then no real prediction can be made
+    if len(opponent_history) < (n_moves + 1):
+        return 'P', 0
+    
+    if q_table:
+        Q = q_table[0]
+    else:
+        Q = initialize_q_table(n_states)
+        q_table.append(Q)
+
+    # get the last n+1 moves
+    # the first n moves define the state for training, the last move is outcome that should be predicted (action)
+    move_sequence_training =[opponent_history[i] for i in range(-n_moves-1 , -1)]
+    correct_action = opponent_history[-1]
+    update_q_table(Q, move_sequence_training, correct_action, possible_n_moves_sequences)
+
+    last_steps = [opponent_history[i] for i in range(-n_moves , 0)]
+    calculated_move, calculated_move_prob =  predict_move(Q, last_steps, possible_n_moves_sequences)
+
+    return IDEAL_RESPONSE[calculated_move], calculated_move_prob
 
 
 # Use default empty list values for opponent_history and q_table as this gives static access to one list respectively that can be updated
 # even outside the life cycle of the player function.
 # This works as default values are initialized once (at first call of the function) and lists are mutable so that when the list is changed
 # it is still the same object the initialized variable is referencing.
-def player(prev_play, opponent_history = [], player_history = [], q_table = []):
+def player(prev_play: str, opponent_history: list[str] = [], player_history: list[str] = [], q_table: list[np.ndarray] = []):
     if prev_play:
         opponent_history.append(prev_play)
     else:
@@ -88,7 +121,7 @@ def player(prev_play, opponent_history = [], player_history = [], q_table = []):
         player_history = []
         q_table = []
 
-    calculated_move = q_learning_enemy_moves(opponent_history, n_moves = 3, q_table = q_table)
+    calculated_move, calculated_move_prob = q_learning_enemy_moves(opponent_history, q_table = q_table, n_moves = 3)
     player_history.append(calculated_move)
     return calculated_move
 
